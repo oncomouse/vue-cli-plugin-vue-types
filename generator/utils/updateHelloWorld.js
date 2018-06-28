950dffafaaabebf40fe2ea80a611da777a2a0113
oncomouse <oncomouse@gmail.com>
2018-06-28 13:55:46 -0500

    Use when instead of cond

--- generator/utils/updateHelloWorld.js
+++ generator/utils/updateHelloWorld.js
@@ -1,19 +1,21 @@
 const {
   compose,
-  cond,
   has,
   prop,
   replace,
   tap,
+  when,
 } = require('ramda');
 
-const updateHelloWorld = tree => cond([
-  [has('src/components/HelloWorld.vue'), compose(
+const updateHelloWorld = tree => when(
+  has('src/components/HelloWorld.vue'),
+  compose(
     tap((xs) => { tree['src/components/HelloWorld.vue'] = xs; }), // eslint-disable-line no-param-reassign
     replace('export', 'import VueTypes from \'vue-types\';\n\nexport'),
     replace('msg: String', 'msg: VueTypes.string'),
     prop('src/components/HelloWorld.vue'),
-  )],
-])(tree);
+  ),
+)(tree);
+
 
 module.exports = updateHelloWorld;
